cff535769dc42b5adf4e69e62349a5fb3f71f1c5
N3Cr0N <n3cr0n@list.ru>
2019-02-08 22:37:30 +0100

    Update main_sidebar.php

--- application/modules/backend/views/templates/main_sidebar.php
+++ application/modules/backend/views/templates/main_sidebar.php
@@ -70,7 +70,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/chartjs"><i class="fa fa-circle-o"></i> ChartJS</a></li>
             <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
             <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
             <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
