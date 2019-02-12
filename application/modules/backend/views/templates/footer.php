b9c4a9b04c0f56986ac86cf648cef025ef1c23ac
N3Cr0N <n3cr0n@list.ru>
2019-02-12 19:17:25 +0100

    Update footer.php

--- application/modules/backend/views/templates/footer.php
+++ application/modules/backend/views/templates/footer.php
@@ -64,6 +64,27 @@ $.widget.bridge('uibutton', $.ui.button);
 <script src="<?php echo base_url(); ?>assets/backend/AdminLTE/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url(); ?>assets/backend/AdminLTE/dist/js/demo.js"></script>
+<script>
+  $(document).ready(function () {
+    $('.sidebar-menu').tree()
+  })
+</script>
+<!-- PACE -->
+<script src="<?php echo base_url(); ?>assets/backend/AdminLTE/bower_components/PACE/pace.min.js"></script>
+<!-- Pace page script -->
+<script type="text/javascript">
+  // To make Pace works on Ajax calls
+  $(document).ajaxStart(function () {
+    Pace.restart()
+  })
+  $('.ajax').click(function () {
+    $.ajax({
+      url: '#', success: function (result) {
+        $('.ajax-content').html('<hr>Ajax Request Completed !')
+      }
+    })
+  })
+</script>
 <!-- ChartJS -->
 <script src="<?php echo base_url(); ?>assets/backend/AdminLTE/bower_components/chart.js/Chart.js"></script>
 <!-- AdminLTE dashboard2 demo (This is only for demo purposes) -->
