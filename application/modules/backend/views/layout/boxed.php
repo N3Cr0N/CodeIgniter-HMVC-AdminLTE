8eb71e21f9ba166bbbdb381c0e0a55ef720923ec
N3Cr0N <n3cr0n@list.ru>
2019-02-12 20:43:09 +0100

    Update boxed.php

--- application/modules/backend/views/layout/boxed.php
+++ application/modules/backend/views/layout/boxed.php
@@ -35,7 +35,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="../../index2.html" class="logo">
+    <a href="<?php echo base_url(); ?>backend/index2" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
@@ -236,11 +236,11 @@
             <li><a href="<?php echo base_url(); ?>backend/top_nav"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
             <li class="active"><a href="<?php echo base_url(); ?>backend/boxed"><i class="fa fa-circle-o"></i> Boxed</a></li>
             <li><a href="<?php echo base_url(); ?>backend/fixed"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/collapsed_sidebar"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
           </ul>
         </li>
         <li>
-          <a href="../widgets.html">
+          <a href="<?php echo base_url(); ?>backend/widgets">
             <i class="fa fa-th"></i> <span>Widgets</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-green">new</small>
@@ -256,10 +256,10 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="../charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="../charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="../charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/chartjs"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/morris"><i class="fa fa-circle-o"></i> Morris</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/flot"><i class="fa fa-circle-o"></i> Flot</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/inline"><i class="fa fa-circle-o"></i> Inline charts</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -271,12 +271,12 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="../UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="../UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-            <li><a href="../UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="../UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="../UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/general"><i class="fa fa-circle-o"></i> General</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/icons"><i class="fa fa-circle-o"></i> Icons</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/buttons"><i class="fa fa-circle-o"></i> Buttons</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/sliders"><i class="fa fa-circle-o"></i> Sliders</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/timeline"><i class="fa fa-circle-o"></i> Timeline</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/modals"><i class="fa fa-circle-o"></i> Modals</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -287,9 +287,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-            <li><a href="../forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-            <li><a href="../forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/general"><i class="fa fa-circle-o"></i> General Elements</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/advanced"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/editors"><i class="fa fa-circle-o"></i> Editors</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -300,12 +300,12 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-            <li><a href="../tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/simple"><i class="fa fa-circle-o"></i> Simple tables</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/data"><i class="fa fa-circle-o"></i> Data tables</a></li>
           </ul>
         </li>
         <li>
-          <a href="../calendar.html">
+          <a href="<?php echo base_url(); ?>backend/calendar">
             <i class="fa fa-calendar"></i> <span>Calendar</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-red">3</small>
@@ -314,7 +314,7 @@
           </a>
         </li>
         <li>
-          <a href="../mailbox/mailbox.html">
+          <a href="<?php echo base_url(); ?>backend/mailbox">
             <i class="fa fa-envelope"></i> <span>Mailbox</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-yellow">12</small>
@@ -331,15 +331,15 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
-            <li><a href="../examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
-            <li><a href="../examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
-            <li><a href="../examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="../examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-            <li><a href="../examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-            <li><a href="../examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-            <li><a href="../examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a href="../examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/invoice"><i class="fa fa-circle-o"></i> Invoice</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/profile"><i class="fa fa-circle-o"></i> Profile</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/login"><i class="fa fa-circle-o"></i> Login</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/register"><i class="fa fa-circle-o"></i> Register</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/lockscreen"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/error_404"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/error_500"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/blank"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+            <li><a href="<?php echo base_url(); ?>backend/pace"><i class="fa fa-circle-o"></i> Pace Page</a></li>
           </ul>
         </li>
         <li class="treeview">
