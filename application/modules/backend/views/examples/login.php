3a0e840bdf6f7419d7674c9db973646a9f85deb3
N3Cr0N <n3cr0n@list.ru>
2019-02-12 19:17:42 +0100

    Update login.php

--- application/modules/backend/views/examples/login.php
+++ application/modules/backend/views/examples/login.php
@@ -30,13 +30,13 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="<?php echo base_url(); ?>backend/index2"><b>Admin</b>LTE</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="<?php echo base_url(); ?>backend/index2" method="post">
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -71,7 +71,7 @@
     <!-- /.social-auth-links -->
 
     <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <a href="<?php echo base_url(); ?>backend/register" class="text-center">Register a new membership</a>
 
   </div>
   <!-- /.login-box-body -->
