fbea6e6c52186ef7245d533b664783bac382684b
N3Cr0N <ByteKingz@mail.ru>
2019-02-18 18:49:49 +0100

    Upgrade to AdminLTE 2.4.9 and Upgrade bootstrap to 3.4.1

--- application/modules/backend/views/templates/footer.php
+++ application/modules/backend/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 <div class="pull-right hidden-xs">
-<b>Version</b> 2.4.8
+<b>Version</b> 2.4.9
 </div>
 <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
 reserved.
